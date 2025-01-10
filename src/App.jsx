59ccbd0e8b70b7a327dd817bb48d3245a9929e81
lavanya <lavanyachinta6@gmail.com>
2025-01-10 10:38:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>     
+      <p className="bg-blue-500 text-white p-4 rounded-lg">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
